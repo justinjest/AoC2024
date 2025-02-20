09d8979e6348c77f53ea1862f70b9dd97d4e5d1a
justinjest <jest.cayaban@gmail.com>
2025-02-20 11:40:53 +0900

    Finished day 3 part 2

--- 03/main.go
+++ 03/main.go
@@ -23,6 +23,17 @@ func findAll(input []byte) ([][]byte) {
 	matches := re.FindAll(input, -1)
 	return matches 
 }
+func findDo(input []byte) ([][]int) {
+	re := regexp.MustCompile("do\\(\\)")	
+	matches := re.FindAllIndex(input, -1)
+	return matches
+}
+
+func findDont(input []byte) ([][]int) {
+	re := regexp.MustCompile("don't\\(\\)")
+	matches := re.FindAllIndex(input, -1)
+	return matches
+}
 
 func multiply(input []byte) (int, error) {
 	nums := input[4: len(input) -1]
@@ -39,6 +50,44 @@ func multiply(input []byte) (int, error) {
 	return res, nil
 }
 
+func listStartOnly(input [][]int) ([]int) {
+	output := make([]int, 0)
+	for i := 0; i < len(input); i++ {
+		output = append(output, input[i][0])
+	}
+	return output
+}
+func splitData(input []byte, startPoints []int, endPoints[]int) ([]byte) {
+	start := 0
+	end := endPoints[0]	
+	data := input[start:end]
+	length := len(input)
+	lenDo := len(startPoints)
+	lenDont := len(endPoints)
+	fmt.Printf("length %v, do %v, dont %v\n", length, lenDo, lenDont)
+	fmt.Printf("%s\n", data)
+
+	for i := 0; i < lenDo; i++ {
+		if startPoints[i] > end {
+			end = nextLargest(endPoints, startPoints[i])
+			if end == 0 {
+				end = length
+			}
+			start = startPoints[i]
+			data =append(data, input[start:end]...)
+		}
+	}
+	return data 
+}
+
+func nextLargest(data []int, val int) int {
+	for i := 0; i < len(data); i++ {
+		if data[i] > val {
+			return data[i]
+		}
+	}
+	return 0
+}
 func main() {
 	input := "input.txt"
 	data , err := reader(input)
@@ -46,7 +95,10 @@ func main() {
 		log.Fatalf("Error reading file %v: %v", input, err)
 	}
 	ans := 0
-	matches := findAll(data)
+	doLocs := listStartOnly(findDo(data))
+	dontLocs:=  listStartOnly(findDont(data))
+	dataCommands := splitData(data, doLocs, dontLocs)
+	matches := findAll(dataCommands)
 		for i := 0; i < len(matches); i++ {	
 		res, err:= multiply(matches[i])
 		if err != nil {
@@ -55,6 +107,7 @@ func main() {
 		ans += res
 		fmt.Printf("%v\n", res)
 	}
+
 	fmt.Println(ans)
 }
 
